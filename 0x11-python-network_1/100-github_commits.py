4a716e020f8e8ca9a46da6ecd1b9cfd2d9fe9151
Happy <srgatoiscool@gmail.com>
2020-04-14 01:51:35 -0500

    add adv

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+"""
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    link = "https://api.github.com/repos/{}/{}/commits".format(
+        argv[1], argv[2])
+    rest = requests.get(link)
+
+    if "json" not in rest.headers.get('content-type'):
+        print("Not a valid JSON")
+    data = rest.json()
+    values = 0
+    for rest in data:
+        if values > 9:
+            break
+        print(rest.get('sha') + ': ' + rest.get('commit').get('author').get('name'))
