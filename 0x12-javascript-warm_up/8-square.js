8dd116c9f94c066951cf5c9a3f6d7c9a56238ce8
Feli <srgatoiscool@gmail.com>
2020-05-05 17:31:40 +0000

    add

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,12 +1,14 @@
 #!/usr/bin/node
 const args = process.argv;
 
-if (!parseInt(args[2])) return console.log('Missing size');
-
-for (let x = parseInt(args[2]); x > 0; x--) {
-  let size = '';
-  for (let x = parseInt(args[2]); x > 0; x--) {
-    size += 'X';
+if (!parseInt(args[2])) {
+  console.log('Missing size');
+} else {
+  for (let timesy = parseInt(args[2]); timesy > 0; timesy--) {
+    let numX = '';
+    for (let timesx = parseInt(args[2]); timesx > 0; timesx--) {
+      numX += 'X';
+    }
+    console.log(numX);
   }
-  console.log(size);
 }
