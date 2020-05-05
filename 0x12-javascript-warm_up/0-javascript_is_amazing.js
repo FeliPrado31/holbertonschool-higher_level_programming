4449562eb61bb7f3b05900b652171b7fd19125ee
Feli <srgatoiscool@gmail.com>
2020-05-05 17:18:13 +0000

    add

--- 0x12-javascript-warm_up/0-javascript_is_amazing.js
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 // Write a script that prints “Javascript is amazing”:
 
-const JavascriptIsAmazing = "Javascript is amazing";
+const JavascriptIsAmazing = 'Javascript is amazing';
 console.log(JavascriptIsAmazing);
